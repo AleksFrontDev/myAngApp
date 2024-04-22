cfd5b3a0c97089dc93f743614f2c2c45dcd37ff0
Aleksandr Generalov <aleksandr.generalov@softgamings.com>
2024-04-22 17:41:15 +0200

    added recipe list and tryed to use directly and no data

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.module';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrl: './recipes-list.component.css'
+  styleUrl: './recipes-list.component.css',
 })
-export class RecipesListComponent {
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('a test Recipe', 'This is a simply test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505')
+  ];
+  constructor() {}
 
+  ngOnInit(): void {}
 }
