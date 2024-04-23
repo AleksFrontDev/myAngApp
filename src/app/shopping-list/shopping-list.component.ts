377d36885126827b8334c4bc597a13f8af36e418
Aleksandr Generalov <aleksandr.generalov@softgamings.com>
2024-04-23 15:08:43 +0200

    added ingredient_module and shopping list template

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.module';
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples',5),
+    new Ingredient('Tomatos',10),
+    new Ingredient('Cucumbers',7)
+  ];
 
   constructor () {
 
